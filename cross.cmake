2e72b3c6b743ffbb4b60956f3b99403bbc7c78a1
Andy Huang <andyspider@126.com>
2015-02-11 19:46:06 +0800

    fix cross.cmake

--- cross.cmake
+++ cross.cmake
@@ -2,14 +2,11 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler, change this if needed
-#SET(CMAKE_C_COMPILER "cross_complier_cc")     # Change this
-#SET(CMAKE_CXX_COMPILER "cross_complier_c++")   # Change this
-SET(CMAKE_C_COMPILER "/home/andy/Ecovacs/cross_compile/arm-guidebot/gcc-linaro/bin/arm-linux-gnueabi-gcc")     # Change this
-SET(CMAKE_CXX_COMPILER "/home/andy/Ecovacs/cross_compile/arm-guidebot/gcc-linaro/bin/arm-linux-gnueabi-g++")   # Change this
+SET(CMAKE_C_COMPILER "cross_complier_cc")     # Change this
+SET(CMAKE_CXX_COMPILER "cross_complier_c++")   # Change this
 
 # where is the target environment, change this to the root system for cross compiling 
-#SET(CMAKE_FIND_ROOT_PATH "location_of_cross_compile_rootsys")   # Change this
-SET(CMAKE_FIND_ROOT_PATH "/tmp/rootsys")   # Change this
+SET(CMAKE_FIND_ROOT_PATH "location_of_cross_compile_rootsys")   # Change this
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
